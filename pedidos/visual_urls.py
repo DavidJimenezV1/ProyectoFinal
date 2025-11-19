e505aa50ebb629f40fdbea1013fabfd8a284403b
DavidJimenez <josephykevin12@gmail.com>
2025-11-18 19:09:47 -0500

    feat(pedidos/ui): agregar UI Bootstrap para lista y edición, forms y templates

--- pedidos/visual_urls.py
+++ pedidos/visual_urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PedidoDetailView, descargar_pedido_pdf
+from . import views
 
-# Este app_name es crucial para que el botón funcione
 app_name = 'pedidos'
 
 urlpatterns = [
-    path('<int:pk>/', PedidoDetailView.as_view(), name='detalle_pedido'),
-    path('<int:pk>/descargar-pdf/', descargar_pedido_pdf, name='descargar_pedido_pdf'),
+    path('', views.lista_pedidos, name='lista_pedidos'),
+    path('pedido/<int:pk>/', views.PedidoDetailView.as_view(), name='detalle_pedido'),
+    path('pedido/<int:pk>/editar/', views.editar_pedido, name='editar_pedido'),
+    path('pedido/<int:pk>/descargar-pdf/', views.descargar_pedido_pdf, name='descargar_pedido_pdf'),
 ]
